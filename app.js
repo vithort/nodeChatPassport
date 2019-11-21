8a8c76b99cfd899c7f382e7f9526ed7cd3de833e
vithort <vithort@gmail.com>
2019-11-21 16:01:11 -0300

    Atualizado
    Ok

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,14 +24,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+require('./auth')(passport);
+app.use(session({
+  store: new MongoStore({
+    db: global.db,
+    ttl: 30 * 60 // = 30 minutos de sessão
+  }),
+  secret: '123',//configure um segredo seu aqui
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('login', { message: null });
 });
 
-router.get('/chat', function(req, res){
-  res.render('chat', {});
+router.get('/chat', authenticationMiddleware(), function (req, res) {
+  res.render('chat', { username: req.user.username });
 });
+
+function authenticationMiddleware() {
+  return function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/login?fail=true')
+  }
+}
+
+router.get('/login', function (req, res) {
+  if (req.query.fail)
+    res.render('login', { message: 'Usuário e/ou senha incorretos!' });
+  else
+    res.render('login', { message: null });
+});
+
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/chat', failureRedirect: '/login?fail=true' })
+);
 
 module.exports = router;
